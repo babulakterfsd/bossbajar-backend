cbfa27c18f0dd2d23efc26fda7a5e261952f4006
Babul Akter <babulakterfsd@gmail.com>
2021-11-01 12:08:52 +0600

    firebase admin added

--- index.js
+++ index.js
@@ -1,69 +1,102 @@
-const express = require('express');
-const { MongoClient } = require('mongodb');
-require('dotenv').config();
-const cors = require('cors');
+const express = require("express");
+const { MongoClient } = require("mongodb");
+require("dotenv").config();
+const cors = require("cors");
+var admin = require("firebase-admin");
 
 const app = express();
 const port = process.env.PORT || 5000;
+
+//firebase admin initialization
+var serviceAccount = require("./bossbajar-a742b-firebase-adminsdk-w6ort-b9b501c0b7.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
 
 //middleware
 app.use(cors());
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ooo4k.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+async function verifyToken(req, res, mext) {
+  if (req.headers.authorization.startsWith("Bearer ")) {
+    const idToken = req.headers.authorization.split("Bearer ")[1];
+    console.log('inside separte function', idToken);
+  }
+  next();
+}
 
 async function run() {
-    try{
-       await client.connect();
-       const database = client.db('online_shop');
-       const productCollection = database.collection('products');
-       const orderCollection = database.collection('orders');
+  try {
+    await client.connect();
+    const database = client.db("online_shop");
+    const productCollection = database.collection("products");
+    const orderCollection = database.collection("orders");
 
-       //get products api
-       app.get('/products', async(req,res) => {
-          const cursor = productCollection.find({});
-          const page = req.query.page;
-          const size = parseInt(req.query.size);
-          let products;
-          const count = await cursor.count();
-          if(page) {
-              products = await cursor.skip(page * size).limit(size).toArray()
-          } else {
-            const products = await cursor.toArray();
-          }
-          res.send({
-              count,
-              products
-          });
-       })
+    //get products api
+    app.get("/products", async (req, res) => {
+      const cursor = productCollection.find({});
+      const page = req.query.page;
+      const size = parseInt(req.query.size);
+      let products;
+      const count = await cursor.count();
+      if (page) {
+        products = await cursor
+          .skip(page * size)
+          .limit(size)
+          .toArray();
+      } else {
+        const products = await cursor.toArray();
+      }
+      res.send({
+        count,
+        products,
+      });
+    });
 
-       //use post to get data by keys
-       app.post('/products/byKeys', async(req,res) => {
-           const keys = req.body;
-           const query = {key: {$in: keys }}
-           const products = await productCollection.find(query).toArray();
-           res.send(products)
-       })
+    //use post to get data by keys
+    app.post("/products/byKeys", async (req, res) => {
+      const keys = req.body;
+      const query = { key: { $in: keys } };
+      const products = await productCollection.find(query).toArray();
+      res.send(products);
+    });
 
-       //add orders api
-       app.post('/orders', async(req,res) => {
-           const order = req.body;
-           const result = await orderCollection.insertOne(order)
-           res.send(result)
-       })
+    //get Orders
+    app.get("/orders", verifyToken, async (req, res) => {
+      let query = {};
+      const email = req.query.email;
+      if (email) {
+        query = { email: email };
+      }
+      const cursor = orderCollection.find(query);
+      const orders = await cursor.toArray();
+      res.json(orders);
+    });
 
-    }
-    finally {
-    //    await client.close(); 
-    }
+    //add orders api
+    app.post("/orders", async (req, res) => {
+      const order = req.body;
+      order.createdAt = new Date();
+      const result = await orderCollection.insertOne(order);
+      res.send(result);
+    });
+  } finally {
+    //    await client.close();
+  }
 }
-run().catch(console.dir)
+run().catch(console.dir);
 
-app.get('/', (req,res) => {
-    res.send('bossbajar server is running');
-})
+app.get("/", (req, res) => {
+  res.send("bossbajar server is running");
+});
 
 app.listen(port, () => {
-    console.log('server running at port', port);
-})
+  console.log("server running at port", port);
+});
